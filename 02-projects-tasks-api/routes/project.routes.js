2f767cd139484ff3930bfe0cd6569b326d0109f4
oliveiramnando <oliveiramnando@gmail.com>
2025-08-18 14:21:13 -0400

    isOwner check on projects

--- 02-projects-tasks-api/routes/project.routes.js
+++ 02-projects-tasks-api/routes/project.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const isOwner =  require('../middleware/checkOwnership.js');
 const { listProjects, readProject, createProject, updateProject, deleteProject } = require('../controllers/project.controllers.js');
 
 router.get('/', listProjects);
 router.get('/:ProjectId', readProject);
-router.post('/', createProject);
-router.put('/:ProjectId', updateProject);
-router.delete('/:ProjectId', deleteProject);
+router.post('/', isOwner, createProject);
+router.put('/:ProjectId', isOwner, updateProject);
+router.delete('/:ProjectId', isOwner, deleteProject);
 
 module.exports = router;
