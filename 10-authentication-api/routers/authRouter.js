7ddc5277dfc167410205620af58fcf67c44bd013
oliveiramnando <oliveiramnando@gmail.com>
2025-08-20 00:21:55 -0400

    authRouter

---
+++ 10-authentication-api/routers/authRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('../controllers/authController.js');
+
+router.post('/signup', authController.signup);
+
+module.exports = router;
