c3400d1fd810dc318f0a4cb21dac8474ff2164fc
oliveiramnando <oliveiramnando@gmail.com>
2025-08-23 16:23:16 -0400

    changePasswordSchema

--- 10-authentication-api/middlewares/validator.js
+++ 10-authentication-api/middlewares/validator.js
@@ -20,3 +20,8 @@ exports.acceptCodeSchema = Joi.object({
     }),
     providedCode: Joi.number().required()
 })
+
+exports.changePasswordSchema = Joi.object({
+    newPassword: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+    oldPassword: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
+})
