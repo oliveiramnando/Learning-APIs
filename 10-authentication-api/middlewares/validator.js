bb1e814374a054a43acd9c2771a1b6fcba226c4b
oliveiramnando <oliveiramnando@gmail.com>
2025-08-23 00:51:38 -0400

    sign in schema

--- 10-authentication-api/middlewares/validator.js
+++ 10-authentication-api/middlewares/validator.js
@@ -4,6 +4,12 @@ exports.signupSchema = Joi.object({
     email: Joi.string().min(6).max(60).required().email({
         tlds: { allowed: ['com', 'net'] }
     }),
-    password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
-    
+    password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
+})
+
+exports.signinSchema = Joi.object({
+    email: Joi.string().min(6).max(60).required().email({
+        tlds: { allowed: ['com', 'net'] }
+    }),
+    password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
 })
