3561ec830ccd886df3f465f6ebe23560023ccee2
oliveiramnando <oliveiramnando@gmail.com>
2025-08-23 00:50:01 -0400

    signin router

--- 10-authentication-api/routers/authRouter.js
+++ 10-authentication-api/routers/authRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const authController = require('../controllers/authController.js');
 
 router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
 
 module.exports = router;
