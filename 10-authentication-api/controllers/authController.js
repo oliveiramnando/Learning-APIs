726a705d3f30379325b3d15e634bef6101856953
oliveiramnando <oliveiramnando@gmail.com>
2025-08-23 01:44:46 -0400

    send verification code

--- 10-authentication-api/controllers/authController.js
+++ 10-authentication-api/controllers/authController.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { signupSchema, signinSchema } = require('../middlewares/validator');
-const { doHash, doHashValidation } = require('../utils/hashing');
+const { doHash, doHashValidation, hmacProcess } = require('../utils/hashing');
 const User = require('../models/usersModel');
-const { exist } = require('joi');
+const transport = require('../middlewares/sendMail');
 
 exports.signup = async (req,res) => {
     const { email, password } = req.body;
@@ -43,7 +43,7 @@ exports.signin = async (req,res) => {
 
         const existingUser = await User.findOne({ email }).select('+password');
         if (!existingUser) {
-            return res.status(401).json({ success: false, message: "User doesn't exist!" });
+            return res.status(404).json({ success: false, message: "User doesn't exist!" });
         }
         const result = await doHashValidation(password, existingUser.password)
         if (!result) {
@@ -67,4 +67,37 @@ exports.signin = async (req,res) => {
 
 exports.signout = async (req,res) => {
     res.clearCookie('Authorization').status(200).json({ success:true, message: "Logged out successfully" });
+};
+
+exports.sendVerificationCode = async (req,res) => {
+    const { email } = req.body;
+    try {
+        const existingUser = await User.findOne({ email })
+        if (!existingUser) {
+            return res.status(404).json({ success: false, message: "User doesn't exist!" });
+        }
+        if (existingUser.verified) {
+            return res.status(400).json({ success: false, message: "Already Verified!" });
+        }
+
+        const codeValue = Math.floor(Math.random() * 1000000).toString();
+        let info = await transport.sendMail({
+            from: process.env.NODE_CODE_EMAIL_ADDRESS,
+            to: existingUser.email,
+            subject: "Verification Code",
+            html: '<h1>' + codeValue + '<h1>'
+        });
+
+        if (info.accepted[0] === existingUser.email) {
+            const hashedCodeValue = hmacProcess(codeValue, process.env.HMAC_VERIFICATION_CODE_SECRET)
+            existingUser.verificationCode = hashedCodeValue;
+            existingUser.verificationCodeValidation = Date.now();
+            await existingUser.save();
+            return res.status(200).json({ success: true, message: "Code sent!"});
+        }
+        return res.status(400).json({ success: false, message: "Code did not send!"})
+
+    } catch (error) {
+        console.log(error);
+    }
 }
