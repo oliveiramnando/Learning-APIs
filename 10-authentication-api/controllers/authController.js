428ff605a154f9245092ad918565d730dfef087a
oliveiramnando <oliveiramnando@gmail.com>
2025-08-23 00:50:36 -0400

    sign in controller: made jwt and cookie

--- 10-authentication-api/controllers/authController.js
+++ 10-authentication-api/controllers/authController.js
@@ -1,6 +1,8 @@
-const { signupSchema } = require('../middlewares/validator');
-const { doHash } = require('../utils/hashing');
+const jwt = require('jsonwebtoken');
+const { signupSchema, signinSchema } = require('../middlewares/validator');
+const { doHash, doHashValidation } = require('../utils/hashing');
 const User = require('../models/usersModel');
+const { exist } = require('joi');
 
 exports.signup = async (req,res) => {
     const { email, password } = req.body;
@@ -26,6 +28,35 @@ exports.signup = async (req,res) => {
             success: true, message: "Account has been created successfully", result
         });
         
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.signin = async (req,res) => {
+    const { email, password } = req.body;
+    try {
+        const { error, value } = signinSchema.validate({ email, password });
+        if (error) {
+            return res.status(401).json({ success:false, message: error.details[0].message });
+        } 
+
+        const existingUser = await User.findOne({ email }).select('+password');
+        if (!existingUser) {
+            return res.status(401).json({ success: false, message: "User doesn't exist!" });
+        }
+        const result = await doHashValidation(password, existingUser.password)
+        if (!result) {
+            return res.status(401).json({ success: false, message: "Invalid Credentials" });
+        }
+        const token = jwt.sign({
+                userId: existingUser._id,
+                email: existingUser.email,
+                verified: existingUser.verified,
+            }, process.env.TOKEN_SECRET
+        );
+        res.cookie('Authorization', 'Bearer ' + token, { expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production'}).json({ success: true, token, message: "Log in successful"});
+
     } catch (error) {
         console.log(error);
     }
