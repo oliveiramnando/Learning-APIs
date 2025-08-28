13741af080d98de934dcda5b8ab4c5d940056791
oliveiramnando <oliveiramnando@gmail.com>
2025-08-27 22:01:25 -0400

    update post

--- 10-authentication-api/controllers/postsController.js
+++ 10-authentication-api/controllers/postsController.js
@@ -27,12 +27,14 @@ exports.getPosts = async (req,res) => {
 exports.singlePost = async (req,res) => {
     const { _id } = req.query;
     try {
-        const result = await Post.findOne({ _id }).populate({
+        const existingPost = await Post.findOne({ _id }).populate({
             path: 'userId',
             select: 'email'
         }); // sorts at created time; skip some posts if page incrementsl; limit to postPErPage; and querying data
+        if (!existingPost) {
+            return res.status(404).json({ success:false, message: "Post not found" });
+        }
         res.status(200).json({ success: true, message: "Single Post", data:result });
-
 
     } catch (error) {
         console.log(error);
@@ -54,6 +56,38 @@ exports.createPost = async (req,res) => {
             userId
         });
         return res.status(201).json({ success:true, message: "Post created", data: result });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+exports.updatePost = async (req,res) => {
+    const { title, description } = req.body;
+    const { userId } = req.user;
+    const { _id } = req.query;
+    
+    try {
+        const { error, value } = createPostSchema.validate({ title, description, userId }); //can use this schema
+        if (error) {
+            return res.status(401).json({ success:false, message: error.details[0].message });
+        } 
+
+        const existingPost = await Post.findOne({ _id });
+        if (!existingPost) {
+            return res.status(403).json({ success:false, message: "Post not found" });
+        }
+
+        if (existingPost.userId.toString() !== userId) {
+            return res.status(401).json({ success:false, message: "Unauthorized Access" });
+        }
+
+        existingPost.title = title;
+        existingPost.description = description
+
+        const result = await existingPost.save();
+
+        return res.status(200).json({ success:true, message: "Post Updated", data: result });
     } catch (error) {
         console.log(error);
     }
--- 10-authentication-api/routers/postsRouter.js
+++ 10-authentication-api/routers/postsRouter.js
@@ -8,9 +8,7 @@ router.get('/all-posts', postsController.getPosts);
 router.get('/single-post', postsController.singlePost);
 router.post('/create-post', identifier, postsController.createPost);
 
-// router.put('/update-post', identifier, authController.sendVerificationCode);
-// router.delete('/delete-post', identifier, authController.verifyVerificationCode);
-
-// module.exports = router;
+router.put('/update-post', identifier, postsController.updatePost);
+// router.delete('/delete-post', identifier, postsController.deletePost);
 
 module.exports = router;
