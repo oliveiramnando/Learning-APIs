28ab47ab6779b47169f4ea908c1cb1acaf282dee
oliveiramnando <oliveiramnando@gmail.com>
2025-08-28 17:32:00 -0400

    signin controller

--- 1-authentication/11-rbac-permissions-api/controllers/authController.js
+++ 1-authentication/11-rbac-permissions-api/controllers/authController.js
@@ -1,11 +1,13 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel.js');
 const hashing = require('../utils/hashing.js');
-const { signupSchema } = require('../middlewares/validator.js');
+const validator = require('../middlewares/validator.js');
 
 exports.signup = async (req,res) => {
     const { email, password } = req.body;
     try {
-        const { error } = signupSchema.validate({ 
+        const { error } = validator.signUpInSchema.validate({ 
             email, 
             password 
         });
@@ -13,9 +15,6 @@ exports.signup = async (req,res) => {
             return res.status(401).json({ success:false, message: error.details[0].message });
         }
 
-        if (!email || !password) {
-            return res.status(400).json({ success: false, message: "Please provide all fields"});
-        }
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ success: false, message: "User already Exists!"});
@@ -29,9 +28,45 @@ exports.signup = async (req,res) => {
 
         const result = await newUser.save();
         result.password = undefined;
-        return res.status(201).json({ success: true, message: "New User Created!", result});
-        
+        return res.status(201).json({ success: true, message: "New User Created!", result });
+
     } catch (error) {   
+        console.log(error);
+    }
+};
+
+exports.signin = async (req,res) => {
+    const { email, password } = req.body;
+    try {
+        const { error } = validator.signUpInSchema.validate({
+            email,
+            password
+        });
+        if (error) {
+            return res.status(401).json({ success:false, message: error.details[0].message });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(404).json({ success: false, message: "User not found, please sign up"});
+        }
+
+        const result = await hashing.compareHash(password, existingUser.password);
+        if (!result) {
+            return res.status(401).json({ success: false, message: "Invalid Credentials" });  
+        }
+
+        const token = jwt.sign({
+                id: existingUser._id,
+                email: existingUser.email,
+            },process.env.TOKEN_SECRET,
+            {
+                expiresIn: '8h'
+            }
+        );
+        res.cookie('Authorization', 'Bearer ' + token, { expires: new Date(Date.now() + 8 * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV === 'production' }).json({ success: true, message: "Login successful!"});
+
+    } catch (error) {
         console.log(error);
     }
 }
