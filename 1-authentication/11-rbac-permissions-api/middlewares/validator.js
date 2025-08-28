58b4ce5f2cf18bc4b8e8bdb7ab9f2b86ac1a2792
oliveiramnando <oliveiramnando@gmail.com>
2025-08-28 14:40:31 -0400

    signupSchema

---
+++ 1-authentication/11-rbac-permissions-api/middlewares/validator.js
@@ -0,0 +1,10 @@
+const Joi = require('joi');
+
+exports.signupSchema = Joi.object({
+    email: Joi.string().min(6).max(60).required().email({
+        tlds: { allowed: ['com', 'net', 'edu']}
+    }),
+    password: Joi.string().required().pattern(new RegExp(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/'
+    ))
+});
