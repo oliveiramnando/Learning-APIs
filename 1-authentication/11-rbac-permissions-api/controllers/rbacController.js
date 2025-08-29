22b136ec25ea58c5b1c218594d5d2df11f928d1e
oliveiramnando <oliveiramnando@gmail.com>
2025-08-28 21:13:26 -0400

    skeleton

---
+++ 1-authentication/11-rbac-permissions-api/controllers/rbacController.js
@@ -0,0 +1,26 @@
+const User = require('../models/userModel.js');
+
+
+exports.listPermissions = async (req,res) => {
+    
+};
+
+exports.listRoles = async (req,res) => {
+    
+};
+
+exports.createRole = async (req,res) => {
+    
+};
+
+exports.updatePermissions = async (req,res) => {
+    
+};
+
+exports.setRole = async (req,res) => {
+    
+};
+
+exports.setUserPermissions = async (req,res) => {
+    
+};
