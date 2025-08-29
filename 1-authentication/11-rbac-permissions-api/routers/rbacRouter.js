596fa89896a4381023ced9c99d0152394f8a2742
oliveiramnando <oliveiramnando@gmail.com>
2025-08-28 21:11:38 -0400

    rbac router outline

--- 1-authentication/11-rbac-permissions-api/routers/rbacRouter.js
+++ 1-authentication/11-rbac-permissions-api/routers/rbacRouter.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const rbacController = require('../controllers/rbacController.js');
+const { identifier } = require('../middlewares/identification.js');
+const { authorizeRole } = require('../middlewares/roleMiddleware.js');
 
 const router = express.Router();
 
-// router.post('/signup')
-// router.post('/signin')
-// router.get('/me')
-// router.post('/logout')
+router.get('/permissions', identifier, authorizeRole("Admin"), rbacController.listPermissions);            // list canonical permissions.
+router.get('/roles', identifier, authorizeRole("Admin"), rbacController.listRoles);                  // list roles with permission keys.
+router.post('/roles', identifier, authorizeRole("Admin"), rbacController.createRole);                  // create role (key, description, permissions[])
+router.patch('/roles/:key', identifier, authorizeRole("Admin"), rbacController.updatePermissions);            // add/remove permission keys.
+router.patch('/users/:id/role', identifier, authorizeRole("Admin"), rbacController.setRole);        // set user role.
+router.patch('/users/:id/permissions', identifier, authorizeRole("Admin"), rbacController.setPermissions); // grant/revoke direct permission keys.
 
 module.exports = router;
