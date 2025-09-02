c1f9a4df5767532778eef1bde564e10fdff29ca1
oliveiramnando <oliveiramnando@gmail.com>
2025-09-02 13:46:30 -0400

    update permissions for role router

--- 1-authentication/11-rbac-permissions-api/routers/rbacRouter.js
+++ 1-authentication/11-rbac-permissions-api/routers/rbacRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/permissions', identifier, authorizeRole("admin"), rbacController.listPermissions);            // list canonical permissions.
 router.get('/roles', identifier, authorizeRole("admin"), rbacController.listRoles);                  // list roles with permission keys.
 router.post('/roles', identifier, authorizeRole("admin"), rbacController.createRole);                  // create role (key, description, permissions[])
-// router.patch('/roles/:key', identifier, authorizeRole("Admin"), rbacController.updatePermissions);            // add/remove permission keys.
+router.patch('/roles/permissions', identifier, authorizeRole("admin"), rbacController.updatePermissions);            // add/remove permission keys.
 router.patch('/users/:id/role', identifier, authorizeRole("admin"), rbacController.setRole);        // set user role.
 router.patch('/users/:id/permissions', identifier, authorizeRole("admin"), rbacController.setPermissions); // grant/revoke direct permission keys.
 
